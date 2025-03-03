6085f6970d9eba407130ce95c3eadf6341ced18b
PELERIN SHAWN <spelerin797@u-001-15.univ-tln.fr>
2025-03-03 15:19:11 +0100

    ajout autre planetes sur nvx deplacement

--- src/main/java/fr/utln/jmonkey/tutorials/Projet/SystemeSolaire.java
+++ src/main/java/fr/utln/jmonkey/tutorials/Projet/SystemeSolaire.java
@@ -79,7 +79,7 @@ public class SystemeSolaire extends SimpleApplication {
 
 		// Lumière d'ambiance faible
 		AmbientLight ambient = new AmbientLight();
-		ambient.setColor(ColorRGBA.White.mult(0.05f));
+		ambient.setColor(ColorRGBA.White.mult(0.01f));
 		rootNode.addLight(ambient);
 
 
@@ -93,21 +93,21 @@ public class SystemeSolaire extends SimpleApplication {
 			assetManager
 		));
 
-		// //Mercure
-		// planets.add(new Planet(
-		// 	"Mercure", 0.4f, 10f,
-		// 	List.of(0f, 4.15f, 0f),
-		// 	List.of(0f, 0.0001f, 0f),
-		// 	assetManager
-		// ));
+		//Mercure
+		planets.add(new Planet(
+			"Mercure", 0.4f, 10f,
+			List.of(0f, 4.15f, 0f),
+			List.of(0f, 0.0001f, 0f),
+			assetManager
+		));
 
-		// // Vénus
-		// planets.add(new Planet(
-		// 	"Venus", 0.95f, 18f,
-		// 	List.of(0f, 1.62f, 0f),
-		// 	List.of(0f, -0.00005f, 0f),
-		// 	assetManager
-		// ));
+		// Vénus
+		planets.add(new Planet(
+			"Venus", 0.95f, 18f,
+			List.of(0f, 1.62f, 0f),
+			List.of(0f, -0.00005f, 0f),
+			assetManager
+		));
 
 		//Terre
 		planets.add(new Planet(
@@ -117,88 +117,88 @@ public class SystemeSolaire extends SimpleApplication {
 			assetManager
 		));
 
-		// //Lune
-		// Planet lune = new Planet(
-		// 	"Lune", 0.27f, 3f,
-		// 	List.of(0f, 12.0f, 0f),
-		// 	List.of(0f, 0.1f, 0f),
-		// 	assetManager
-		// );
-		// planets.get(3).addMoon(lune);
+		//Lune
+		Planet lune = new Planet(
+			"Lune", 0.27f, 3f,
+			List.of(0f, 12.0f, 0f),
+			List.of(0f, 0.1f, 0f),
+			assetManager
+		);
+		planets.get(3).addMoon(lune);
 
-		// // Mars
-		// planets.add(new Planet(
-		// 	"Mars", 0.53f, 35f,
-		// 	List.of(0f, 0.53f, 0f),
-		// 	List.of(0f, 0.97f, 0f),
-		// 	assetManager
-		// ));
+		// Mars
+		planets.add(new Planet(
+			"Mars", 0.53f, 35f,
+			List.of(0f, 0.53f, 0f),
+			List.of(0f, 0.97f, 0f),
+			assetManager
+		));
 
-		// //Lunes de Mars Phobos Deimos
-		// Planet phobos = new Planet(
-		// 	"Lune", 0.05f, 2f,
-		// 	List.of(0f, 4.0f, 0f),
-		// 	List.of(0f, 0.2f, 0f),
-		// 	assetManager
-		// );
-		// planets.get(4).addMoon(phobos);
+		//Lunes de Mars Phobos Deimos
+		Planet phobos = new Planet(
+			"Lune", 0.05f, 2f,
+			List.of(0f, 4.0f, 0f),
+			List.of(0f, 0.2f, 0f),
+			assetManager
+		);
+		planets.get(4).addMoon(phobos);
 	
-		// Planet deimos = new Planet(
-		// 	"Lune", 0.04f, 4f,
-		// 	List.of(0f, 6.0f, 0f),
-		// 	List.of(0f, 0.3f, 0f),
-		// 	assetManager
-		// );
-		// planets.get(4).addMoon(deimos);
+		Planet deimos = new Planet(
+			"Lune", 0.04f, 4f,
+			List.of(0f, 6.0f, 0f),
+			List.of(0f, 0.3f, 0f),
+			assetManager
+		);
+		planets.get(4).addMoon(deimos);
 
-		// //Jupiter
-		// planets.add(new Planet(
-		// 	"Jupiter", 2.2f, 60f,
-		// 	List.of(0f, 0.084f, 0f),
-		// 	List.of(0f, 2.5f, 0f),
-		// 	assetManager
-		// ));
+		//Jupiter
+		planets.add(new Planet(
+			"Jupiter", 2.2f, 60f,
+			List.of(0f, 0.084f, 0f),
+			List.of(0f, 2.5f, 0f),
+			assetManager
+		));
 
-		// //Lunes de Jupiter Io Europa
-		// Planet io = new Planet(
-		// 	"Lune", 0.3f, 4f,
-		// 	List.of(0f, 2.0f, 0f),  // Rotation autour de Jupiter
-		// 	List.of(0f, 0.1f, 0f),  // Rotation sur elle-même
-		// 	assetManager
-		// );
-		// planets.get(5).addMoon(io);
+		//Lunes de Jupiter Io Europa
+		Planet io = new Planet(
+			"Lune", 0.3f, 4f,
+			List.of(0f, 2.0f, 0f),  // Rotation autour de Jupiter
+			List.of(0f, 0.1f, 0f),  // Rotation sur elle-même
+			assetManager
+		);
+		planets.get(5).addMoon(io);
 	
-		// Planet europa = new Planet(
-		// 	"Lune", 0.25f, 8f,
-		// 	List.of(0f, 3.0f, 0f),  // Rotation autour de Jupiter
-		// 	List.of(0f, 0.1f, 0f),  // Rotation sur elle-même
-		// 	assetManager
-		// );
-		// planets.get(5).addMoon(europa);
+		Planet europa = new Planet(
+			"Lune", 0.25f, 8f,
+			List.of(0f, 3.0f, 0f),  // Rotation autour de Jupiter
+			List.of(0f, 0.1f, 0f),  // Rotation sur elle-même
+			assetManager
+		);
+		planets.get(5).addMoon(europa);
 
-		// //Saturne
-		// planets.add(new Planet(
-		// 	"Saturne", 3.45f, 80f,
-		// 	List.of(0f, 0.034f, 0f),
-		// 	List.of(0f, 2.3f, 0f),
-		// 	assetManager
-		// ));
+		//Saturne
+		planets.add(new Planet(
+			"Saturne", 3.45f, 80f,
+			List.of(0f, 0.034f, 0f),
+			List.of(0f, 2.3f, 0f),
+			assetManager
+		));
 
-		// //Uranus
-		// planets.add(new Planet(
-		// 	"Uranus", 2.0f, 100f,
-		// 	List.of(0f, 0.0119f, 0f),
-		// 	List.of(0f, 1.4f, 0f),
-		// 	assetManager
-		// ));
+		//Uranus
+		planets.add(new Planet(
+			"Uranus", 2.0f, 100f,
+			List.of(0f, 0.0119f, 0f),
+			List.of(0f, 1.4f, 0f),
+			assetManager
+		));
 
-		// //Neptune
-		// planets.add(new Planet(
-		// 	"Neptune", 2.9f, 120f,
-		// 	List.of(0f, 0.006f, 0f),
-		// 	List.of(0f, 1.5f, 0f),
-		// 	assetManager
-		// ));
+		//Neptune
+		planets.add(new Planet(
+			"Neptune", 2.9f, 120f,
+			List.of(0f, 0.006f, 0f),
+			List.of(0f, 1.5f, 0f),
+			assetManager
+		));
 
 		for(Planet p : planets){
 			rootNode.attachChild(p.getRoot());
